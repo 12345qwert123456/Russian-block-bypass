f5f94be7118f4fa0f2e68f8eb1136b795018f729
Nikitin Timofey <63474188+12345qwert123456@users.noreply.github.com>
2024-05-26 15:45:04 +0300

    Add keenetic rules

---
+++ keenetic.bat
@@ -0,0 +1,51 @@
+@REM Instagram
+
+route ADD 31.13.0.0       MASK 255.255.0.0     0.0.0.0
+route ADD 45.64.0.0       MASK 255.255.0.0     0.0.0.0
+route ADD 66.220.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 69.63.0.0       MASK 255.255.0.0     0.0.0.0
+route ADD 69.171.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 74.119.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 87.245.208.0    MASK 255.255.255.0   0.0.0.0
+route ADD 99.84.0.0       MASK 255.255.0.0     0.0.0.0
+route ADD 102.132.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 102.221.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 103.4.0.0       MASK 255.255.0.0     0.0.0.0
+route ADD 129.134.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 142.250.0.0     MASK 255.254.0.0     0.0.0.0
+route ADD 147.75.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 149.154.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 157.240.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 163.70.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 163.77.128.0    MASK 255.255.128.0   0.0.0.0
+route ADD 163.114.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 164.163.191.64  MASK 255.255.255.192 0.0.0.0
+route ADD 173.252.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 179.60.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 185.60.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 185.89.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 199.201.0.0     MASK 255.255.0.0     0.0.0.0
+route ADD 204.15.20.0     MASK 255.255.252.0   0.0.0.0
+route ADD 213.102.128.0   MASK 255.255.255.0   0.0.0.0
+
+@REM Medium
+
+route ADD 162.159.152.4   MASK 255.255.255.255 0.0.0.0
+route ADD 162.159.153.4   MASK 255.255.255.255 0.0.0.0
+
+@REM ChatGPT
+
+route ADD 104.18.0.0      MASK 255.255.0.0     0.0.0.0
+route ADD 172.64.0.0      MASK 255.255.0.0     0.0.0.0
+
+@REM Microsoft Copilot
+
+route ADD 2.16.100.0      MASK 255.255.254.0   0.0.0.0
+route ADD 2.19.96.0       MASK 255.255.254.0   0.0.0.0
+route ADD 2.23.209.0      MASK 255.255.255.0   0.0.0.0
+route ADD 23.15.178.0     MASK 255.255.255.0   0.0.0.0
+
+@REM Rutracker
+
+route ADD 104.21.32.39    MASK 255.255.255.255 0.0.0.0
+route ADD 172.67.182.196  MASK 255.255.255.255 0.0.0.0
